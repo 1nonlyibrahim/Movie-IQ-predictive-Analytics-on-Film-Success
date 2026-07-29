99474f794db7000810b080324bb6e16e4a5c4518
Ibrahim Khan <ibrahim.utube2008@gmail.com>
2026-07-29 19:00:19 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -182,10 +182,10 @@ st.markdown(f"""
 if not st.session_state.uploaded:
 
     uploaded_file = st.file_uploader(
-        "Drag & Drop your CSV here or Click to Upload",
-        type=["csv"],
-        help="Only CSV files are supported."
-    )
+    "Drag & Drop your CSV here or Click to Upload",
+    type=["csv"],
+    help="Only CSV files are supported."
+)
 
 if uploaded_file and not st.session_state.uploaded:
 
