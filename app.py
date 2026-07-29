bed8a3b48c1b833d2335c5002d1188374c5c2086
Ibrahim Khan <ibrahim.utube2008@gmail.com>
2026-07-30 01:50:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -521,7 +521,7 @@ if st.session_state.validation_complete:
 
     st.sidebar.metric(
         "Outlier Count",
-        int(outliers)
+        int(outliers),
         help="Outliers are unusually high or low values detected using the IQR (Interquartile Range) method. They are reported for analysis but are not removed automatically."
 )
 
